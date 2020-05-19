6a56fc3108aa81ce9f1e93d2424d9df703308ee2
n-orris <evannorris99@gmail.com>
2020-05-19 03:19:37 -0700

    "initial upload of partially complete index.html styles.css scripts.js and image folder"

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// selectors
+const container = document.querySelector('.container');
+const body = document.querySelector('.body');
+
+//created elements
+const div = document.createElement('div');
+
+// additions
+// appending to Dom
+body.appendChild(image);
+
+
+
