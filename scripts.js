88894e6cc892f6bd1e70641a040582e6aa81ec24
n-orris <evannorris99@gmail.com>
2020-05-21 00:35:22 -0700

    "fixes to spacing and comments"

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
+// Selectors
 const container = document.getElementById("container");
 const multibtn = document.getElementById("multibtn");
 const erasebtn = document.getElementById("erasebtn");
 const colorPicker = document.getElementById("colorPicker");
-// grid making function
+
+
+// grid making method
 function makeRows(rows, cols) {
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty('--grid-cols', cols);
@@ -10,7 +13,8 @@ function makeRows(rows, cols) {
     for (let i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
-    };
+    }
+    ;
 };
 
 makeRows(12, 12);
@@ -34,13 +38,15 @@ function change(color) {
 change("black");
 
 // listener for color wheel
-if(colorPicker){
+if (colorPicker) {
     colorPicker.addEventListener("change", watchColorPicker, false);
 }
-// if user changes color wheel this function applies the change
+
+// if user changes color wheel this method applies the change
 function watchColorPicker(event) {
     change(event.target.value)
-    }
+}
+
 // on click chooses random color for you
 multibtn.addEventListener("click", () => {
     change('#' + Math.floor(Math.random() * 16777215).toString(16));
@@ -49,9 +55,3 @@ multibtn.addEventListener("click", () => {
 erasebtn.addEventListener("click", () => {
     change("white");
 })
-
-
-
-
-
-
